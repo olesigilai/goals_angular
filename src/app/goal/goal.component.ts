bcaf713c81ca69be50d896885be654e49d49711a
olesigilai <ole_sigilai@yahoo.com>
2021-01-29 08:15:53 +0300

    alert on deletion button clicked

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -15,6 +15,9 @@ export class GoalComponent implements OnInit {
     new Goal(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
     new Goal(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2030,3,14)),
   ];
+  toggleDetails(index:any){
+    this.goals[index].showDescription = !this.goals[index].showDescription;
+  }
   deleteGoal(isComplete:any, index:any){
     if (isComplete) {
       let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
